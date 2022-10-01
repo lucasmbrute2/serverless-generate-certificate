01e6785b2011deea012309cf5bca197b7849fe6a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-01 16:40:09 -0300

    feat: implemented handlebars compile

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,27 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 import { document } from "../utils/dynamobdbClient"
+import * as handlebars from "handlebars"
+import { join } from "path"
+import { readFileSync } from "fs"
+import * as dayjs from "dayjs"
+
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string
+}
+
+interface ITemplate extends ICreateCertificate {
+  medal?: string;
+  date?: string;
+}
+
+const compile = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificates.hbs");
+  const html = readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -28,6 +45,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ":id": id
     }
   }).promise()
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png")
+  const medal = readFileSync(medalPath, "base64")
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal
+  }
+
+  const content = await compile(data)
 
   return {
     statusCode: 201,
