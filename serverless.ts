27e6faff6bff47d7d991b07d988cea8eb482d61a
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2022-10-04 09:20:21 -0300

    feat: Implemented aws lambda

--- serverless.ts
+++ serverless.ts
@@ -1,12 +1,14 @@
 import type { AWS } from '@serverless/typescript';
 
+
 const serverlessConfiguration: AWS = {
   service: 'certificate-ignite',
-  frameworkVersion: '3',
+  frameworkVersion: '2',
   plugins: ['serverless-esbuild', 'serverless-dynamodb-local', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -15,32 +17,35 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
+    lambdaHashingVersion: '20201221',
     iamRoleStatements: [
       {
-        Effect: "Allow",
-        Action: ["dynamodb:*"],
-        Resource: ["*"]
-      }
-    ]
+        Effect: 'Allow',
+        Action: ['dynamodb:*'],
+        Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:*'],
+        Resource: ['*'],
+      },
+    ],
   },
-  // import the function via paths
+  package: { individually: false, include: ['./src/templates/**'] },
   functions: {
     generateCertificate: {
-      handler: "src/functions/generateCertificate.handler",
-      timeout: 10,
+      handler: 'src/functions/generateCertificate.handler',
       events: [
         {
           http: {
-            path: "generateCertificate",
-            method: "post",
-
+            path: 'generateCertificate',
+            method: 'post',
             cors: true,
-          }
-        }
-      ]
-    }
+          },
+        },
+      ],
+    },
   },
-  package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
@@ -51,43 +56,43 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: ['chrome-aws-lambda'],
     },
     dynamodb: {
       stages: ['dev', 'local'],
       start: {
         port: 8000,
         inMemory: true,
-        migrate: true
-      }
-    }
+        migrate: true,
+      },
+    },
   },
-
   resources: {
     Resources: {
       dbCertificateUsers: {
-        Type: "AWS::DynamoDB::Table",
+        Type: 'AWS::DynamoDB::Table',
         Properties: {
-          TableName: "users_certificate",
+          TableName: 'users_certificate',
           ProvisionedThroughput: {
             ReadCapacityUnits: 5,
-            WriteCapacityUnits: 5
+            WriteCapacityUnits: 5,
           },
           AttributeDefinitions: [
             {
-              AttributeName: "id",
-              AttributeType: "S"
-            }
+              AttributeName: 'id',
+              AttributeType: 'S',
+            },
           ],
           KeySchema: [
             {
-              AttributeName: "id",
-              KeyType: "HASH"
-            }
-          ]
-        }
-      }
-    }
-  }
+              AttributeName: 'id',
+              KeyType: 'HASH',
+            },
+          ],
+        },
+      },
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -3,8 +3,10 @@ import { document } from "../utils/dynamobdbClient"
 import * as handlebars from "handlebars"
 import { join } from "path"
 import { readFileSync } from "fs"
-import * as dayjs from "dayjs"
-
+import dayjs from "dayjs"
+//@ts-ignore
+import * as chromium from "chrome-aws-lambda"
+import { S3 } from "aws-sdk"
 
 interface ICreateCertificate {
   id: string;
@@ -18,7 +20,7 @@ interface ITemplate extends ICreateCertificate {
 }
 
 const compile = async (data: ITemplate) => {
-  const filePath = join(process.cwd(), "src", "templates", "certificates.hbs");
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
   const html = readFileSync(filePath, "utf-8");
 
   return handlebars.compile(html)(data)
@@ -28,16 +30,6 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
   const TableName = 'users_certificate';
 
-  await document.put({
-    TableName,
-    Item: {
-      id,
-      name,
-      grade,
-      created_at: new Date().getTime()
-    }
-  }).promise()
-
   const response = await document.query({
     TableName,
     KeyConditionExpression: "id = :id",
@@ -45,6 +37,20 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ":id": id
     }
   }).promise()
+
+  const haveSomeCertificate = response.Items[0];
+
+  if (!haveSomeCertificate) {
+    await document.put({
+      TableName,
+      Item: {
+        id,
+        name,
+        grade,
+        created_at: new Date().getTime()
+      }
+    }).promise()
+  }
 
   const medalPath = join(process.cwd(), "src", "templates", "selo.png")
   const medal = readFileSync(medalPath, "base64")
@@ -58,9 +64,41 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   }
 
   const content = await compile(data)
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  })
+
+  const page = await browser.newPage()
+  await page.setContent(content)
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? "./certificate.pdf" : null
+  })
+
+  await browser.close()
+
+  const s3 = new S3();
+
+  await s3.putObject({
+    Bucket: "certificate-nodejs-ignite-soruz",
+    Key: `${id}.pdf`,
+    Body: pdf,
+    ContentType: "application/pdf",
+    ACL: "public-read"
+  }).promise()
 
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0])
+    body: JSON.stringify({
+      message: "Certificado criado com sucesso!",
+      url: `https://certificate-nodejs-ignite-soruz.s3.amazonaws.com/${id}.pdf`,
+      payload: response.Items[0]
+
+    })
   }
 }
